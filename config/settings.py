2722756a45c4466ae706167e3a88cb3501f57254
AHZ1386 <100196498+AHZ1386@users.noreply.github.com>
2024-10-05 22:43:08 +0330

    fix the database problem on deploy

--- config/settings.py
+++ config/settings.py
@@ -85,14 +85,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'database'/'db.sqlite3' ,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database'/'db.sqlite3' ,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'databaseyvn_db',
+        'USER': 'postgres',
+        'PASSWORD': 'fcNneGHKnpGTe6gxH23f',
+        'HOST': 'http://management-asiatech.runflare.com',
+        'PORT': '32317'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
