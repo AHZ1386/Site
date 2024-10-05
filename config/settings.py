5ff015968ac32d250a63805ad056d8209def45dd
AHZ1386 <100196498+AHZ1386@users.noreply.github.com>
2024-10-05 22:18:13 +0330

    fix the database problem

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/db.sqlite3',
     }
 }
 
